36dc87fc1fcab5ddf21530ac9ba812f5c883943e
Matias2196 <propet66@gmail.com>
2023-02-28 19:37:47 -0300

    Acualizacion 1

--- lippia-web-sample-project-master/src/main/java/lippia/web/constants/GoogleConstants.java
+++
@@ -1,8 +0,0 @@
-package lippia.web.constants;
-
-public class GoogleConstants {
-    public static final String SHOP_ID = "id:menu-item-40";
-    public static final String HOME_ID = "xpath://*[@id='content']/nav/a";
-    public static final String CERRAR = "xpath://*[@id=\'dismiss-button\']";
-
-}
